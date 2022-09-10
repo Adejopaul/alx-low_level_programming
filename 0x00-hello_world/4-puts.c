45a04b533937a81ef79bd353d073551488255113
Adejopaul <Raymondmatiu@icloud.com>
2022-09-10 10:10:57 -0700

    glass

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,9 @@
-#include ‹stdio.h>
-
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-this program prints
-"Programing is like building a multilingual puzzle
-Return: 0
-*/
-int main(void)
-{
-  
-     puts(")"Programming is like building a multilingual puzzle*):
-     return (0);
-}     
+#include <stdio.h>                                                                                                                                   
+/**                                                                                                                                                  
+*main-Entry point                                                                                                                                    
+*Return: 0 if successful                                                                                                                             
+*/                                                                                                                                                   
+int main(void)                                                                                                                                       
+{                                                                                                                                                    
+puts("\"Programming is like building a multilingual puzzle");                                                                                        
+return (0);                                                                                                                                          
