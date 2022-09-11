b8a41a378ae6f89f16f3247ff2ae103185cd3ba3
Adejopaul <Raymondmatiu@icloud.com>
2022-09-11 08:12:29 -0700

    coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o ${CFILE//'.c'/'.s'}
+*include «stdio.h»
+main - print the string in the put function
+Description; using the main function
+Return: •
+this program prints "Programing is like building a multilingual puzzle
+int main(vold)
+{
+char C:
+int 1;
+long 11;
+long long 111;
+float f;
+printf("Size of a char: Xld byte(s) \n", sizeof(c));
+printf ("Size of an int: *ld byte(s) \n", sizeof(1));
+printf("Size of a long int: *ld byte(s) in", sizeof(1));
+printf ("Size of a long long int: *ld byte(s) in"
+•, sizeof(111)):
+printf ("Size of a float: Xld byte(s) In°, sizeof(f));
+return (0);
+AC
