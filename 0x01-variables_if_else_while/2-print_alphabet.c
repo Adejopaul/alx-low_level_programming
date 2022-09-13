041d194f4ac2067cd04c6949098a750a83f3f742
Adejopaul <Raymondmatiu@icloud.com>
2022-09-12 17:37:28 -0700

    not stoping

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+
+* main - Entry
+
+*
+
+* Return: 0 if successful
+
+*/
+
+
+
+int main(void)
+
+{
+
+	char start;
+
+
+
+	for (start = 'a'; start  <= 'z'; start++)
+
+	{
+
+		putchar(start);
+
+	} putchar('\n');
+
+return (0);
+
+}
