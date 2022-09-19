a0bff05e1fd12a6106072851da5c331e50fcba1c
Adejopaul <Raymondmatiu@icloud.com>
2022-09-19 15:27:21 -0700

    damn

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,21 +1,34 @@
 #ifndef MAIN_H
-#define MAIN.H
 
-/**
-*void prototypes()
-*int prototypes()
-*/
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void pits_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
 
-#endif /*MAIN.H*/
+int _putchar(char c);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *s);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+
+
+#endif /* MAIN_H */
