2d2b0e4442b84146c81e5100ee798d86b8b6e713
Adejopaul <Raymondmatiu@icloud.com>
2022-09-22 11:54:48 -0700

    m

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,39 +7,23 @@
 *@n: the number of bytes to use from src.
 *Return: string.
 */
-
 char *_strncpy(char *dest, char *src, int n)
-
 {
 
 int i = 0, k = 0;
-
-
-
 while (n > k)
-
 {
-
 if (src[k] == '\0')
-
 {
-
 for (; k < n; k++)
-
 {
-
 dest[i] = '\0';
-
 i++;
-
 }
-
 }
 
 else
-
 {
-
 dest[i] = src[k];
 
 k++;
