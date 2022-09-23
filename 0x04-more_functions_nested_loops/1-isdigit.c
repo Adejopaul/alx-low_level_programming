c0c467a9fec1d4c68d5ccf6a59c8955640d21a2f
Adejopaul <Raymondmatiu@icloud.com>
2022-09-22 20:23:52 -0700

    m

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,25 @@
 #include "main.h"
-/**
-* _isdigit	-	checks if an input is a digit
-* @c:			The input to be tested
-* Return:	-	1 if c is a digit else, 0
-*/
-int _isdigit(int c)
-{
-int item;
 
-for (item = '0'; item <= '9'; item++)
-	{
-	if (item == c)
-		return (1);
-	}
-return (0);
+/**
+* print_alphabet - a function that prints the alphabet, in lowercase
+*
+* Return: Always 0 (Succes)
+*/
+void print_alphabet(void)
+{
+
+int n;
+
+
+
+for (n = 'a'; n <= 'z'; n++)
+
+{
+
+_putchar(n);
+
+}
+
+_putchar('\n');
+
 }
