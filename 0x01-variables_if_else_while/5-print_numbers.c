7ccbab135ca0bb33756bf0ea935c8d2da1bbd437
Adejopaul <Raymondmatiu@icloud.com>
2022-09-23 15:06:17 -0700

    m

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-
-
-
-
-
 /**
-
 * main - entry
-
 *
-
 * Return: 0 if successful (always)
-
 */
-
-
-
 int main(void)
-
 {
-
-	char number = '0';
-
-
-
-	for (number = '0'; number <= '9'; number++)
-
-		putchar(number);
-
-	putchar('\n');
-
-	return (0);
-
+char number = '0';
+for (number = '0'; number <= '9'; number++)
+putchar(number);
+putchar('\n');
+return (0);
 }
