8ce4d1d18daadd1e9e53a5e5d55970217f2c3bdf
Adejopaul <Raymondmatiu@icloud.com>
2022-09-22 21:17:00 -0700

    ok

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,21 @@
 */
 void print_triangle(int size)
 {
-	int height, base;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+int height, base;
+if (size <= 0)
+_putchar('\n');
+else
 {
-		for (height = 1; height <= size; ++height)
+for (height = 1; height <= size; ++height)
 {
-			for (base = 1; base <= size; ++base)
+for (base = 1; base <= size; ++base)
 {
-				if ((height + base) <= size)
-					_putchar(' ');
-				else
-					_putchar('#');
+if ((height + base) <= size)
+_putchar(' ');
+else
+_putchar('#');
 }
-			_putchar('\n');
+_putchar('\n');
 }
 }
