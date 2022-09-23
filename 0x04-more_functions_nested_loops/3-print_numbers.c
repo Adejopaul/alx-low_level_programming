a9c911f6c7f229a8e98f8e40536b2fd0a8b0434c
Adejopaul <Raymondmatiu@icloud.com>
2022-09-22 20:45:16 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers - entry point
+*
+*Returns: 0 if successful
+*/
+void print_numbers(void)
+{
+
+int n;
+
+for (n = 48; n < 58; n++)
+
+{
+
+putchar(n);
+
+}
+
+putchar(10);
+
+}
