26ea65fe768d5cca6c6f315977c81b5fdff73e96
Adejopaul <Raymondmatiu@icloud.com>
2022-09-22 20:54:52 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_line   -  prints a line
+* @n:  Defines the length of line to be printed
+* Return:  Alwasy 0
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+putchar('\n');
+}
+
+else
+
+{
+
+while (n > 0)
+
+{
+
+putchar('_');
+
+n--;
+
+}
+
+putchar('\n');
+
+}
+
+}
